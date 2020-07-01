5c67699dc8bae12bdca4c3c6742e43a4314a07a9
Romina8 <guerrero17romina@gmail.com>
2020-07-01 09:31:39 -0300

    Se inicia el proyecto creando las entities, se agregan anotaciones de la relacion Persona Billetera, haciendo un mapeo bidireccional, el mappedBy se agrega desde donde sale la PK y el JoinColumn donde esta la FK, queda pendiente agregar anotaciones a las demas entities y las relaciones bidireccionales de todas las demas.

---
+++ billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/entities/Persona.java
@@ -0,0 +1,109 @@
+package ar.com.ada.api.billeteravirtual.entities;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "persona")
+public class Persona {
+
+	@Id
+	@Column(name = "persona_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer personaId;
+	private String nombre;
+	
+	@Column(name = "pais_id")
+	private Integer paisId;
+	
+	@Column(name = "tipo_documento_id")
+	private Integer tipoDocumentoId;
+	private String documento;
+	
+	@Column(name = "fecha_nacimiento")
+	private Date fechaNacimiento;
+
+	@OneToOne(mappedBy = "persona", cascade = CascadeType.ALL)
+	private Usuario usuario;
+
+	@OneToOne(mappedBy = "persona", cascade = CascadeType.ALL)
+	private Billetera billetera;
+
+	public Integer getPersonaId() {
+		return personaId;
+	}
+
+	public void setPersonaId(Integer personaId) {
+		this.personaId = personaId;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Integer getPaisId() {
+		return paisId;
+	}
+
+	public void setPaisId(Integer paisId) {
+		this.paisId = paisId;
+	}
+
+	public Integer getTipoDocumentoId() {
+		return tipoDocumentoId;
+	}
+
+	public void setTipoDocumentoId(Integer tipoDocumentoId) {
+		this.tipoDocumentoId = tipoDocumentoId;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+		this.usuario.setPersona(this);
+		;
+	}
+
+	public Billetera getBilletera() {
+		return billetera;
+	}
+
+	public void setBilletera(Billetera billetera) {
+		this.billetera = billetera;
+		this.billetera.setPersona(this);
+	}
+
+    
+}
