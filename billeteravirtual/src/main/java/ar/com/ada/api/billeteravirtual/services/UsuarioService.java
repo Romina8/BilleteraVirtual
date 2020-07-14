93461eb5ca400c2be5623d7e7b8b8f1105630755
Romina8 <guerrero17romina@gmail.com>
2020-07-14 18:44:00 -0300

    Se genera los metodos para poder crear un usuario, apartir del service Persona ya que el objeto contenedor es Persona.

--- billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/services/UsuarioService.java
+++ billeteravirtual/src/main/java/ar/com/ada/api/billeteravirtual/services/UsuarioService.java
@@ -1,27 +1,53 @@
 package ar.com.ada.api.billeteravirtual.services;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ar.com.ada.api.billeteravirtual.entities.Persona;
 
 import ar.com.ada.api.billeteravirtual.entities.Usuario;
+import ar.com.ada.api.billeteravirtual.repos.UsuarioRepository;
 
 @Service
 public class UsuarioService {
 
-        /*1.Metodo: Crear Usuario
-    1.1-->Crear una Persona(setearle un usuario)
-    1.2-->crear un usuario
-    1.3-->Crear una billetera(setearle una persona)
-    1.4-->Crear una cuenta en pesos y otra en dolares*/
+  @Autowired
+  UsuarioRepository repo;
 
-    /* 2. Metodo: Iniciar Sesion 
-    2.1-- recibe el username y la password
-    2.2-- vamos a validar los datos
-    2.3-- devolver un verdadero o falso
-    */
+  @Autowired
+  PersonaService personaService;
 
-    public Usuario buscarPorUsername(String username) {
-		return null;
-	}
+  public Usuario buscarPorUsername(String username) {
+      return null;
+  }
 
-	public void login(String username, String password) {
-	}
-    
+  public void login(String username, String password) {
+  }
+
+
+  public Usuario crearUsuario(String nombre, int pais, int tipoDocumento, String documento, Date fechaNacimiento,
+          String email, String password) {
+
+      /*
+       * 1.1-->Crear una Persona(setearle un usuario) 1.2-->crear un usuario
+       * 1.3-->Crear una billetera(setearle una persona) 1.4-->Crear una cuenta en
+       * pesos y otra en dolares
+       */
+      Persona persona = new Persona();
+      persona.setNombre(nombre);
+      persona.setPaisId(pais);
+      persona.setTipoDocumentoId(tipoDocumento);
+      persona.setDocumento(documento);
+      persona.setFechaNacimiento(fechaNacimiento);
+
+      Usuario usuario = new Usuario();
+      usuario.setUsername(email);
+      usuario.setEmail(email);
+      usuario.setPassword(password);
+
+      persona.setUsuario(usuario);
+
+      personaService.grabar(persona);
+
+      return usuario;
+  }
 }
